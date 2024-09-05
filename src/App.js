51a514a969d23555826b990a2f027fdc2ea0971f
Tanish Kumar <tanishkr66@gmail.com>
2024-09-06 02:26:15 +0530

    Initialize gpt project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="bg-red-600">
+     <h1 className='text-3xl text-emerald-500'>hello</h1> 
+    </div>
+  );
+}
+
+export default App;
